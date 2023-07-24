966137bada0444b9ee44782bd2347e4691322119
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-07-24 21:06:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
