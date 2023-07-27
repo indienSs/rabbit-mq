45d546b2affe404450425736edd6add389a2cbf0
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-07-27 22:18:22 +0300

    get/post methods

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { messageSender } from "./send";
+import { messageReceiver } from "./receive";
 
 dotenv.config();
 
@@ -9,6 +11,14 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/", (req, res) => {
+  messageSender();
+});
+
+app.get("/", (req, res) => {
+  messageReceiver();
+});
 
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
